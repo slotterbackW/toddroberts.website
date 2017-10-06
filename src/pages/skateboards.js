7eb2515d7193ea2dac56948522453e7fd1cd3a1f
Will Slotterback <will2337@gmail.com>
2017-10-05 21:13:31 -0400

    Typo

--- src/pages/skateboards.js
+++ src/pages/skateboards.js
@@ -15,7 +15,7 @@ const Skateboards = (props) => (
     <div className="title-container">
       <h1>Custom Skateboards</h1>
     </div>
-    <p>I grew up working on home projects with my dad in our wood shop, and learning how to skateboard on the concrete pad just outside of it. As time went on I figured out that I could combine these two hobbies and started building my own boards. Below are some examples of boards I have made. I cut and shaped the painted boards from pre pressed 7-ply maple blanks and then did the graphics with a combination of spray paint and paint pens. In order to quickly and consistently round the edges of the boards I bought a handheld router and then made a custom guide that could follow the multidimensional curves. I created the chevron board entirely from scratch, including cutting the bottom veneer, glueing and pressing the plies, and shaping.</p>
+    <p>I grew up working on home projects with my dad in our wood shop, and learning how to skateboard on the concrete pad just outside of it. As time went on I figured out that I could combine these two hobbies and started building my own boards. Below are some examples of boards I have made. I cut and shaped the painted boards from pre-pressed 7-ply maple blanks and then did the graphics with a combination of spray paint and paint pens. In order to quickly and consistently round the edges of the boards I bought a handheld router and then made a custom guide that could follow the multidimensional curves. I created the chevron board entirely from scratch, including cutting the bottom veneer, glueing and pressing the plies, and shaping.</p>
     <div className="row">
       <div className="col-12">
         <img src={HoldingBoard} alt="Twist technology in its fully extended form" width="100%"/>
